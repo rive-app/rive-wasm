e2bb465afb0a9122063ee07dcc94a4b3542e2974
matt Sullivan <matt.j.sullivan@gmail.com>
2021-04-13 19:11:41 -0700

    Adds vue example

---
+++ js/examples/vue_example/src/components/Rive.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <canvas ref="canvas" width="400" height="400"></canvas>
+  </div>
+</template>
+
+<script>
+import { Rive, Layout } from 'rive-js'
+
+export default {
+  name: 'Rive',
+  props: {
+    src: String,
+    fit: String,
+    alignment: String
+  },
+  mounted() {
+    new Rive({
+        canvas: this.$refs.canvas,
+        src: this.$props.src,
+        layout: new Layout({
+            fit: this.$props.fit,
+            alignment: this.$props.alignment,
+        }),
+        autoplay: true
+    })
+  }
+}
+</script>
