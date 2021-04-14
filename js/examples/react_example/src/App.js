4645bf07d0b7082017e9b24a04df28107707714e
matt Sullivan <matt.j.sullivan@gmail.com>
2021-04-13 19:11:41 -0700

    Adds vue example

--- js/examples/react_example/src/App.js
+++ js/examples/react_example/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Animation asset="https://cdn.rive.app/animations/off_road_car_v7.riv" fit="contain" alignment="center" />
+        <Animation asset="https://cdn.rive.app/animations/off_road_car_v7.riv" fit="scaleDown" alignment="topLeft" />
         <p>
           Edit the <code>src/Animation.js</code> Rive React component.
         </p>
---
+++ js/examples/vue_example/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Rive 
+      src="https://cdn.rive.app/animations/mech_v7.riv"
+      fit="cover"
+      alignment="center"
+    />
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Rive from './components/Rive.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Rive
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
