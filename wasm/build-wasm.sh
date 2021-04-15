33f87f23557aae001311e783443b131952343766
matt Sullivan <matt.j.sullivan@gmail.com>
2021-04-15 10:06:25 -0700

    Updates canvas build to generate wasm file

--- wasm/build-wasm.sh
+++ wasm/build-wasm.sh
@@ -2,33 +2,6 @@
 
 OUTPUT_DIR=bin/release
 
-if [ $# -ne 1 ]; then
-    echo "usage: build.sh <es6|es5|es6pure|es5pure>"
-    exit 1
-fi
-
-if [ "$1" == "es6" ]; then
-    FILE_EXTENSION=mjs
-    OUTPUT_FILE=rive
-    WASM=1
-elif [ "$1" == "es5" ]; then
-    FILE_EXTENSION=js
-    OUTPUT_FILE=rive
-    WASM=1
-elif [ "$1" == "es6pure" ]; then
-    FILE_EXTENSION=mjs
-    OUTPUT_FILE=rive.pure
-    WASM=0
-elif [ "$1" == "es5pure" ]; then
-    FILE_EXTENSION=js
-    OUTPUT_FILE=rive.pure
-    WASM=0
-else
-    echo "incorrect type: build.sh <es6|es5|es6pure|es5pure>"
-    exit 1
-fi
-
-
 mkdir -p build
 pushd build &>/dev/null
 
@@ -40,7 +13,7 @@ em++ -Oz \
     --closure 0 \
     --bind \
     -g1 \
-    -o $OUTPUT_DIR/$OUTPUT_FILE.$FILE_EXTENSION \
+    -o $OUTPUT_DIR/rive.mjs \
     -s ASSERTIONS=0 \
     -s FORCE_FILESYSTEM=0 \
     -s MODULARIZE=1 \
@@ -48,8 +21,8 @@ em++ -Oz \
     -s STRICT=1 \
     -s ALLOW_MEMORY_GROWTH=1 \
     -s DISABLE_EXCEPTION_CATCHING=1 \
-    -s WASM=$WASM \
-    -s SINGLE_FILE=1 \
+    -s WASM=1 \
+    -s SINGLE_FILE=0 \
     -s EXPORT_NAME="Rive" \
     -s LLD_REPORT_UNDEFINED \
     -DEMSCRIPTEN_HAS_UNBOUND_TYPE_NAMES=0 \
@@ -69,22 +42,6 @@ em++ -Oz \
     ../submodules/rive-cpp/src/shapes/paint/*.cpp \
     ../src/bindings.cpp
 
-awk 'NR==FNR { a[n++]=$0; next }
-/console\.log\("--REPLACE WITH RENDERING CODE--"\);/ { for (i=0;i<n;++i) print a[i]; next }
-1' ../js/renderer.js ./bin/release/$OUTPUT_FILE.$FILE_EXTENSION >./bin/release/rive-combined.$FILE_EXTENSION
-
-if ! command -v terser &>/dev/null; then
-    npm install terser -g
-fi
-terser --compress --mangle -o ./bin/release/$OUTPUT_FILE.min.$FILE_EXTENSION -- ./bin/release/rive-combined.$FILE_EXTENSION
-
-# Encode the wasm binary into string and wrap in js
-# node ../scripts/wasm2str.js ./bin/release/rive.wasm ./bin/release/rive_wasm.js
-
-# copy to publish folder
-cp ./bin/release/rive-combined.$FILE_EXTENSION ../publish/$OUTPUT_FILE.$FILE_EXTENSION
-cp ./bin/release/rive.min.mjs ../publish/$OUTPUT_FILE.min.$FILE_EXTENSION
-# cp ./bin/release/rive.wasm ../publish/rive.wasm
-# cp ./bin/release/rive_wasm.js ../publish/rive_wasm.js
+cp ./bin/release/rive.wasm ../publish/rive.wasm
 
 popd &>/dev/null
