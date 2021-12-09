7a55e8c36c42a903724e86e122310f4891b0b4c6
Luigi Rosso <luigi.rosso@gmail.com>
2021-12-08 17:01:36 -0800

    More build options for light.

--- js/build.sh
+++ js/build.sh
@@ -5,30 +5,13 @@ set -e
 # Sets up the build structure expected by webpack and packs
 
 # create the build dir
-mkdir -p build/lean
 mkdir -p dist
 
 # copy the rive wasm js and type definitions files to build dir
 if [ ! -f ../wasm/publish/rive.mjs ]; then 
     pushd ../wasm
-    ./build.sh
+    ./build-all-wasm.sh
     popd
-fi
-cp ../wasm/publish/rive.mjs ./src/rive_canvas.js
-cp ../wasm/publish/types.d.ts ./src/rive_canvas.d.ts
-cp ../wasm/publish/rive.mjs ./dist/rive_canvas.js
-cp ../wasm/publish/types.d.ts ./dist/rive_canvas.d.ts
-cp ../wasm/publish/rive.lean.mjs ./build/lean/wasm.js
-cp ../wasm/publish/types.d.ts ./build/lean/wasm.d.ts
-
-# Copy the rive runtime file to build dir
-cp src/rive.ts build/lean/
-
-# Replace the import statement at the top
-if [ "$(uname)" == "Darwin" ]; then
-    sed -i '' '1s/.*/import * as rc from '"'"'.\/wasm'"'"';/' build/lean/rive.ts
-else
-    sed -i '1s/.*/import * as rc from '"'"'.\/wasm'"'"';/' build/lean/rive.ts
 fi
 
 # run web-pack
