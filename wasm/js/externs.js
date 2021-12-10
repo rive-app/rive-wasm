58b5a5ca061ba21cc05e8d760c699c5f0f740955
Luigi Rosso <luigi.rosso@gmail.com>
2021-12-10 15:54:42 -0800

    getting rive_light working

--- wasm/js/externs.js
+++ wasm/js/externs.js
@@ -1,15 +1,50 @@
 var Rive = {
-    RenderPaintStyle: { fill: {}, stroke: {} },
-    FillRule: { evenOdd: {}, nonZero: {} },
-    StrokeCap: { butt: {}, round: {}, square: {} },
-    StrokeJoin: { miter: {}, round: {}, bevel: {} },
-    BlendMode: { srcOver: {}, screen: {}, overlay: {}, darken: {}, lighten: {}, colorDodge: {}, colorBurn: {}, hardLight: {}, softLight: {}, difference: {}, exclusion: {}, multiply: {}, hue: {}, saturation: {}, color: {}, luminosity: {} },
+    RenderPaintStyle: {
+        fill: {},
+        stroke: {}
+    },
+    FillRule: {
+        evenOdd: {},
+        nonZero: {}
+    },
+    StrokeCap: {
+        butt: {},
+        round: {},
+        square: {}
+    },
+    StrokeJoin: {
+        miter: {},
+        round: {},
+        bevel: {}
+    },
+    BlendMode: {
+        srcOver: {},
+        screen: {},
+        overlay: {},
+        darken: {},
+        lighten: {},
+        colorDodge: {},
+        colorBurn: {},
+        hardLight: {},
+        softLight: {},
+        difference: {},
+        exclusion: {},
+        multiply: {},
+        hue: {},
+        saturation: {},
+        color: {},
+        luminosity: {}
+    },
     Renderer: {},
     CanvasRenderer: {},
     RenderPath: {},
     RenderPaint: {},
     RenderImage: {},
-    renderFactory: { makeRenderPaint: function () { }, makeRenderPath: function () { }, makeRenderImage: function () { }, },
-    onRuntimeInitialized: function () { }
+    renderFactory: {
+        makeRenderPaint: function () {},
+        makeRenderPath: function () {},
+        makeRenderImage: function () {},
+    },
+    onRuntimeInitialized: function () {},
+    makeRenderer: function () {}
 };
-
