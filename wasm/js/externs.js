c37ba6ae3aeffd7d440d2682e9d07b90454355a0
Luigi Rosso <luigi.rosso@gmail.com>
2021-12-09 20:59:44 -0800

    Add extern.js when compiling to avoid mangling reserved words.

---
+++ wasm/js/externs.js
@@ -0,0 +1,15 @@
+var Rive = {
+    RenderPaintStyle: { fill: {}, stroke: {} },
+    FillRule: { evenOdd: {}, nonZero: {} },
+    StrokeCap: { butt: {}, round: {}, square: {} },
+    StrokeJoin: { miter: {}, round: {}, bevel: {} },
+    BlendMode: { srcOver: {}, screen: {}, overlay: {}, darken: {}, lighten: {}, colorDodge: {}, colorBurn: {}, hardLight: {}, softLight: {}, difference: {}, exclusion: {}, multiply: {}, hue: {}, saturation: {}, color: {}, luminosity: {} },
+    Renderer: {},
+    CanvasRenderer: {},
+    RenderPath: {},
+    RenderPaint: {},
+    RenderImage: {},
+    renderFactory: { makeRenderPaint: function () { }, makeRenderPath: function () { }, makeRenderImage: function () { }, },
+    onRuntimeInitialized: function () { }
+};
+
