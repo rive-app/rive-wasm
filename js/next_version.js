1e260be97f2ac35d472d80825f21e1fb51b21d39
Arthur Vivian <arthur@rive.app>
2021-12-14 12:38:46 +0000

    update next_version script

--- js/next_version.js
+++ js/next_version.js
@@ -1,30 +1,37 @@
 const fs = require('fs');
+const package = require('./package.json');
+const versions = JSON.parse(process.argv[2].trim().replace(/\'/g, '"'));
+const current = package.version;
+const latest = versions[versions.length-1];
 
-let package = require('./package.json');
-let current = package.version.split('.');
-let versions = JSON.parse(process.argv[2].trim().replace(/\'/g, '"'));
+// Returns -1 if first is less than second, 1 if first is greater than second, otherwise 0 if equal.
+function compareVersion(first, second) {
+    // Assumption: only numbers in our versions.
+    const firstParts = first.split('.').map((value) => parseInt(value));
+    const secondParts = second.split('.').map((value) => parseInt(value));
 
-for (let i = 0; i < current.length; i++) {
-    current[i] = Number.parseInt(current[i]);
+    for (let i = 0; i < firstParts.length; i++) {
+        if (secondParts.length === i) {
+            return 1;
+        }
+
+        if (firstParts[i] < secondParts[i]) {
+            return -1
+        } else if (firstParts[i] > secondParts[i]) {
+            return 1;
+        }
+    }
+
+    if (firstParts.length != secondParts.length) {
+        return -1;
+    }
+
+    return 0;
 }
 
-// Set the build number at minimum to the desired one, that way if nothing
-// conflicts, we'll take it.
-let buildNumber = current[2];
-// Iterate all versions and find the max buildNumber that is in use so we can
-// take the next one.
-for (let version of versions) {
-    version = version.split('.');
-    for (let i = 0; i < version.length; i++) {
-        version[i] = Number.parseInt(version[i]);
-    }
-    if (current[0] == version[0] && current[1] == version[1] && buildNumber <= version[2]) {
-        buildNumber = version[2] + 1;
-    }
-
+if (compareVersion(current, latest) <= 0) {
+    const parts = latest.split('.').map((value) => parseInt(value));
+    parts[parts.length-1] = parts[parts.length-1] + 1;
+    package.version = parts.join('.');
+    fs.writeFileSync('./package.json', JSON.stringify(package, null, 2));
 }
-
-current[2] = buildNumber;
-package.version = current.join('.');
-
-fs.writeFileSync('./package.json', JSON.stringify(package, null, 2));
