01d1941c77ff9227021bb4b09d0dbed588453286
Luigi Rosso <luigi.rosso@gmail.com>
2021-12-14 09:41:08 -0800

    Fixing tests.

--- js/jest.config.js
+++ js/jest.config.js
@@ -4,7 +4,8 @@ module.exports = {
     "^.+\\.m?[t|j]sx?$": "babel-jest"
   },
   "moduleNameMapper": {
-    "rive_canvas.mjs": "<rootDir>/dist/rive_canvas_light.mjs"
+    "rive_advanced.mjs": "<rootDir>/npm/canvas_advanced_single/canvas_advanced_single.mjs",
+    "package.json": "<rootDir>/npm/canvas_advanced_single/package.json",
   },
   "transformIgnorePatterns": [
     // Needed to handle ES6 rive-canvas module
