9db4e3ae115ba54b9b7e234d197a0fb946556ce2
Luigi Rosso <luigi.rosso@gmail.com>
2021-12-14 15:49:07 -0800

    Updating version bumping and publishing to work with all new packages.

--- js/publish_all.sh
+++ js/publish_all.sh
@@ -4,7 +4,7 @@ set -e
 # Bump the version number of every npm module in the npm folder.
 for dir in ./npm/*; do
     pushd $dir > /dev/null
-    echo Bumping version of `echo $dir | sed 's:.*/::'`
-    ../../bump_version.sh
+    echo Publishing `echo $dir | sed 's:.*/::'`
+    npm publish $@
     popd > /dev/null
 done
