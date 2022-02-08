7df0faa8f387f6b3ceddeb2717e03eabf8f9d51f
Umberto Sonnino <umberto@rive.app>
2022-02-08 15:06:26 +0000

    Fix linter warnings on script

--- wasm/build_wasm.sh
+++ wasm/build_wasm.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
+set -e
 
 OPTIONS=0
 PREMAKE_FLAGS=
-WD=`pwd`
+WD=$(pwd)
 export EMCC_CLOSURE_ARGS="--externs $WD/js/externs.js"
 while getopts "s:r:" flag; do
     case "${flag}" in
-        s)
-            OPTIONS=$(( OPTIONS + 1 ))
-            PREMAKE_FLAGS+="--single_file "
-            ;;
-        r) 
-            OPTIONS=$(( OPTIONS + 2 ))
-            if [ "${OPTARG}" = "skia" ]; then
-                PREMAKE_FLAGS+="--skia "
-            fi
-            ;;
+    s)
+        OPTIONS=$((OPTIONS + 1))
+        PREMAKE_FLAGS+="--single_file "
+        ;;
+    r)
+        OPTIONS=$((OPTIONS + 2))
+        if [ "${OPTARG}" = "skia" ]; then
+            PREMAKE_FLAGS+="--skia "
+        fi
+        ;;
+    *)
+        # Alert users about unused/wrong flags.
+        echo Unknown option flag "$flag"
     esac
 done
 shift $OPTIONS
 OPTION=$1
-MODE=debug
 
 if [ "$OPTION" = 'help' ]; then
     echo build.sh - build debug library
@@ -32,10 +35,9 @@ elif [ "$OPTION" = "clean" ]; then
     rm -fR ./build
     exit 0
 elif [ "$OPTION" = "tools" ]; then
-    premake5 gmake2 $PREMAKE_FLAGS && AR=emar CFLAGS=-DENABLE_QUERY_FLAT_VERTICES CXXFLAGS=-DENABLE_QUERY_FLAT_VERTICES CC=emcc CXX=em++ make config=release -j7
+    premake5 gmake2 "$PREMAKE_FLAGS" && AR=emar CFLAGS=-DENABLE_QUERY_FLAT_VERTICES CXXFLAGS=-DENABLE_QUERY_FLAT_VERTICES CC=emcc CXX=em++ make config=release -j7
 elif [ "$OPTION" = "release" ]; then
-    MODE=release
-    premake5 gmake2 $PREMAKE_FLAGS && AR=emar CC=emcc CXX=em++ make config=release -j7
+    premake5 gmake2 "$PREMAKE_FLAGS" && AR=emar CC=emcc CXX=em++ make config=release -j7
 else
-    premake5 gmake2 $PREMAKE_FLAGS && AR=emar CC=emcc CXX=em++ make -j7
+    premake5 gmake2 "$PREMAKE_FLAGS" && AR=emar CC=emcc CXX=em++ make -j7
 fi
