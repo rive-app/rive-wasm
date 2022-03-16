2a2503d1bc5df60e13baf98f055fb2a73865311d
Luigi Rosso <luigi.rosso@gmail.com>
2022-03-16 15:05:33 -0700

    Updated skia renderer in js to use shared context.

--- wasm/js/skia_renderer.js
+++ wasm/js/skia_renderer.js
@@ -1,6 +1,51 @@
 Module.onRuntimeInitialized = function () {
-    const makeRenderer = Module.makeRenderer;
-    Module.makeRenderer = function (canvas) {
+    function OffscreenRenderer(canvas) {
+        this._canvas = canvas;
+        this._ctx = canvas.getContext('2d');
+
+        this._realRenderer = _offscreenGL;
+        this['clear'] = function () {
+            const canvas = this._canvas;
+            const glCanvas = _offscreenGL._canvas;
+
+            if (glCanvas.width < canvas.width || glCanvas.height < canvas.height) {
+                glCanvas.width = canvas.width;
+                glCanvas.height = canvas.height;
+            }
+            _offscreenGL.clear();
+        };
+
+        this['save'] = function () {
+            _offscreenGL.save();
+        };
+
+        this['restore'] = function () {
+            _offscreenGL.restore();
+        };
+
+        this['transform'] = function (xform) {
+            _offscreenGL.transform(xform);
+        };
+
+        this['align'] = function (fit, align, from, to) {
+            _offscreenGL.align(fit, align, from, to);
+        };
+
+        this['computeAlignment'] = function (mat2d, fit, align, from, to) {
+            _offscreenGL.computeAlignment(mat2d, fit, align, from, to);
+        };
+
+        this['flush'] = function () {
+            _offscreenGL.flush();
+            const ctx = this._ctx;
+            ctx.globalCompositeOperation = 'copy';
+            ctx.drawImage(_offscreenGL._canvas, 0, 0);
+        };
+    }
+
+    let _offscreenGL = null;
+
+    function makeGLRenderer(canvas) {
         var contextAttributes = {
             'alpha': 1,
             'depth': 1,
@@ -30,7 +75,25 @@ Module.onRuntimeInitialized = function () {
         renderer._width = canvas.width;
         renderer._height = canvas.height;
         return renderer;
+    }
+
+    const makeRenderer = Module.makeRenderer;
+    Module.makeRenderer = function (canvas, useOffScreenRenderer) {
+        if (useOffScreenRenderer) {
+            if (!_offscreenGL) {
+                _offscreenGL = makeGLRenderer(document.createElement('canvas'));
+            }
+            return new OffscreenRenderer(canvas);
+        }
+        return makeGLRenderer(canvas);
     };
+
+    const wasmDraw = Module['Artboard']['prototype']['draw'];
+    Module['Artboard']['prototype']['draw'] = function (renderer) {
+        const realRenderer = renderer._realRenderer;
+        wasmDraw.call(this, realRenderer || renderer);
+    };
+
     const cppClear = Module['WebGLRenderer']['prototype']['clear'];
     Module['WebGLRenderer']['prototype']['clear'] = function () {
         GL.makeContextCurrent(this._handle);
