aa12982e79119a2aba444c0cadc3d7389acbc926
Chris Dalton <chris@rive.app>
2022-03-22 21:19:04 -0600

    Initial work to make js/build.sh support incremental builds
    Builds the various targets in their own directories. Moves the "clean"
    step to a block that can be commented out in order to enable incremental
    builds. Build steps for individual targets can also be commented out now
    that their artifacts persist if we skip the clean step.

--- wasm/build_all_wasm.sh
+++ wasm/build_all_wasm.sh
@@ -35,34 +35,37 @@ mkdir -p ../js/npm/webgl_advanced
 mkdir -p ../js/npm/canvas_advanced_single
 mkdir -p ../js/npm/webgl_advanced_single
 
+# Comment this block to use incremental builds.
+echo
+echo "::::: cleaning all projects"
+echo
+rm -fR ./build
+
 echo 
 echo "::::: building @rive-app/canvas_advanced"
 echo 
-./build_wasm.sh clean
-./build_wasm.sh release
-cp build/bin/release/canvas_advanced.mjs ../js/npm/canvas_advanced/canvas_advanced.mjs
-cp build/bin/release/canvas_advanced.wasm ../js/npm/canvas_advanced/rive.wasm
-cp build/bin/release/canvas_advanced.wasm ../js/npm/canvas/rive.wasm
+OUT_DIR=build/canvas_advanced ./build_wasm.sh release
+cp build/canvas_advanced/bin/release/canvas_advanced.mjs ../js/npm/canvas_advanced/canvas_advanced.mjs
+cp build/canvas_advanced/bin/release/canvas_advanced.wasm ../js/npm/canvas_advanced/rive.wasm
+cp build/canvas_advanced/bin/release/canvas_advanced.wasm ../js/npm/canvas/rive.wasm
 
 echo 
 echo "::::: building @rive-app/canvas_advanced_single"
 echo 
-./build_wasm.sh clean
-./build_wasm.sh -s release
-cp build/bin/release/canvas_advanced_single.mjs ../js/npm/canvas_advanced_single/canvas_advanced_single.mjs
+OUT_DIR=build/canvas_advanced_single ./build_wasm.sh -s release
+cp build/canvas_advanced_single/bin/release/canvas_advanced_single.mjs ../js/npm/canvas_advanced_single/canvas_advanced_single.mjs
 
 echo 
 echo "::::: building @rive-app/webgl_advanced"
 echo 
-./build_wasm.sh clean
-./build_wasm.sh -r skia release
-cp build/bin/release/webgl_advanced.mjs ../js/npm/webgl_advanced/webgl_advanced.mjs
-cp build/bin/release/webgl_advanced.wasm ../js/npm/webgl_advanced/rive.wasm
-cp build/bin/release/webgl_advanced.wasm ../js/npm/webgl/rive.wasm
+OUT_DIR=build/webgl_advanced ./build_wasm.sh -r skia release
+cp build/webgl_advanced/bin/release/webgl_advanced.mjs ../js/npm/webgl_advanced/webgl_advanced.mjs
+cp build/webgl_advanced/bin/release/webgl_advanced.wasm ../js/npm/webgl_advanced/rive.wasm
+cp build/webgl_advanced/bin/release/webgl_advanced.wasm ../js/npm/webgl/rive.wasm
 
 echo 
 echo "::::: building @rive-app/webgl_advanced_single"
 echo 
 ./build_wasm.sh clean
-./build_wasm.sh -r skia -s release
+OUT_DIR=build/webgl_advanced_single ./build_wasm.sh -r skia -s release
 cp build/bin/release/webgl_advanced_single.mjs ../js/npm/webgl_advanced_single/webgl_advanced_single.mjs
