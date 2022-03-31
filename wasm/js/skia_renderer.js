11635ff943624ff9416f0479134898354b9c2f47
Luigi Rosso <luigi.rosso@gmail.com>
2022-03-31 16:41:03 -0700

    Render meshes to canvas2d via offscreen webgl
    This is a preliminary effort. Performance is only decent in Chrome. We
    will probably need to make use of an atlas among other things to try and
    get this to perform.

--- wasm/js/skia_renderer.js
+++ wasm/js/skia_renderer.js
@@ -129,32 +129,6 @@ Module.onRuntimeInitialized = function () {
         return (n + mask) & ~mask;
     }
 
-    // Tracks the max size that has been pushed within a given window of time.
-    // Used as a buffer to avoid thrashing the offscreen canvas size.
-    function MaxRecentSize(milliseconds) {
-        console.assert(milliseconds > 0);
-        const _map = new Map();
-        const _getMax = function(now) {
-            // Return the max size in our window.
-            let maxSize = 0;
-            _map.forEach((expiration, size) => {
-                if (now > expiration) {
-                    _map.delete(size);
-                } else {
-                    maxSize = Math.max(maxSize, size);
-                }
-            });
-            return maxSize;
-        };
-        this.push = function(size) {
-            const now = Date.now();
-            const expiration = now + milliseconds;
-            // Set (or update) the expiration time for this size before calling _getMax().
-            _map.set(size, expiration);
-            return _getMax(now);
-        };
-    }
-
     const _atlasMaxRecentWidth = new MaxRecentSize(1000 /*1 second*/);
     const _atlasMaxRecentHeight = new MaxRecentSize(1000 /*1 second*/);
 
