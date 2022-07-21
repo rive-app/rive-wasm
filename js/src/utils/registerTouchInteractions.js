2b0db330a45585427f7f908edf0715dc0b2a9afa
Maxwell Talbot <talbot.maxwell@gmail.com>
2022-07-21 14:19:16 +0200

    remove mouse events

--- js/src/utils/registerTouchInteractions.js
+++ js/src/utils/registerTouchInteractions.js
@@ -15,51 +15,64 @@ export const registerTouchInteractions = ({
     return;
   }
 
-    const mouseCallback = (event) => {
-      const boundingRect = event.currentTarget.getBoundingClientRect();
+  const mouseCallback = (event) => {
+    const boundingRect = event.currentTarget.getBoundingClientRect();
 
-      const canvasX = event.clientX - boundingRect.left;
-      const canvasY = event.clientY - boundingRect.top;
-      const forwardMatrix = rive.computeAlignment(fit, alignment, {
+    const canvasX = event.clientX - boundingRect.left;
+    const canvasY = event.clientY - boundingRect.top;
+    const forwardMatrix = rive.computeAlignment(
+      fit,
+      alignment,
+      {
         minX: 0,
         minY: 0,
         maxX: boundingRect.width,
         maxY: boundingRect.height,
-      }, artboard.bounds);
-      let invertedMatrix = new rive.Mat2D();
-      forwardMatrix.invert(invertedMatrix);
-      const canvasCoordinatesVector = new rive.Vec2D(canvasX, canvasY);
-      const transformedVector = rive.mapXY(invertedMatrix, canvasCoordinatesVector);
-      const transformedX = transformedVector.x();
-      const transformedY = transformedVector.y();
+      },
+      artboard.bounds
+    );
+    let invertedMatrix = new rive.Mat2D();
+    forwardMatrix.invert(invertedMatrix);
+    const canvasCoordinatesVector = new rive.Vec2D(canvasX, canvasY);
+    const transformedVector = rive.mapXY(
+      invertedMatrix,
+      canvasCoordinatesVector
+    );
+    const transformedX = transformedVector.x();
+    const transformedY = transformedVector.y();
 
-      switch (event.type) {
-        // Pointer moving/hovering on the canvas
-        case 'mousemove': {
-          for (const stateMachine of stateMachines) {
-            stateMachine.pointerMove(transformedX, transformedY);
-          }
-          break;
+    switch (event.type) {
+      // Pointer moving/hovering on the canvas
+      case 'mousemove': {
+        for (const stateMachine of stateMachines) {
+          stateMachine.pointerMove(transformedX, transformedY);
         }
-        // Pointer click initiated but not released yet on the canvas
-        case 'mousedown': {
-          for (const stateMachine of stateMachines) {
-            stateMachine.pointerDown(transformedX, transformedY);
-          }
-          break;
-        }
-        // Pointer click released on the canvas
-        case 'mouseup': {
-          for (const stateMachine of stateMachines) {
-            stateMachine.pointerUp(transformedX, transformedY);
-          }
-          break;
-        }
-        default:
+        break;
       }
-    };
-
-    canvas.addEventListener("mousemove", mouseCallback.bind(this));
-    canvas.addEventListener("mousedown", mouseCallback.bind(this));
-    canvas.addEventListener("mouseup", mouseCallback.bind(this));
+      // Pointer click initiated but not released yet on the canvas
+      case 'mousedown': {
+        for (const stateMachine of stateMachines) {
+          stateMachine.pointerDown(transformedX, transformedY);
+        }
+        break;
+      }
+      // Pointer click released on the canvas
+      case 'mouseup': {
+        for (const stateMachine of stateMachines) {
+          stateMachine.pointerUp(transformedX, transformedY);
+        }
+        break;
+      }
+      default:
+    }
+  };
+  const callback = mouseCallback.bind(this);
+  canvas.addEventListener('mousemove', callback);
+  canvas.addEventListener('mousedown', callback);
+  canvas.addEventListener('mouseup', callback);
+  return () => {
+    canvas.removeEventListener('mousemove', callback);
+    canvas.removeEventListener('mousedown', callback);
+    canvas.removeEventListener('mouseup', callback);
+  };
 };
