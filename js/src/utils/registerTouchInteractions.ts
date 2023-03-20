7f8a23c39d8a7fccf723105a318800bd36c224e1
Zach Plata <plata.zach@gmail.com>
2023-03-20 07:07:29 -0500

    feature: support touch events for listeners

--- js/src/utils/registerTouchInteractions.ts
+++ js/src/utils/registerTouchInteractions.ts
@@ -1,3 +1,15 @@
+import * as rc from "../rive_advanced.mjs";
+
+export interface TouchInteractionsParams {
+  canvas: HTMLCanvasElement | OffscreenCanvas;
+  artboard: rc.Artboard;
+  stateMachines: rc.StateMachineInstance[];
+  renderer: rc.Renderer;
+  rive: rc.RiveCanvas;
+  fit: rc.Fit;
+  alignment: rc.Alignment;
+}
+
 /**
  * Registers mouse move/up/down callback handlers on the canvas to send meaningful coordinates to
  * the state machine pointer move/up/down functions based on cursor interaction
@@ -10,16 +22,44 @@ export const registerTouchInteractions = ({
   rive,
   fit,
   alignment,
-}) => {
-  if (!canvas || !stateMachines.length || !renderer || !rive || !artboard) {
+}: TouchInteractionsParams) => {
+  if (
+    !canvas ||
+    !stateMachines.length ||
+    !renderer ||
+    !rive ||
+    !artboard ||
+    typeof window === "undefined"
+  ) {
     return null;
   }
 
-  const mouseCallback = (event) => {
-    const boundingRect = event.currentTarget.getBoundingClientRect();
+  const processEventCallback = (event: MouseEvent | TouchEvent) => {
+    const boundingRect = (
+      event.currentTarget as HTMLCanvasElement
+    ).getBoundingClientRect();
 
-    const canvasX = event.clientX - boundingRect.left;
-    const canvasY = event.clientY - boundingRect.top;
+    let canvasX;
+    let canvasY;
+    if (
+      ["touchstart", "touchmove"].indexOf(event.type) > -1 &&
+      (event as TouchEvent).touches?.length
+    ) {
+      canvasX = (event as TouchEvent).touches[0].clientX - boundingRect.left;
+      canvasY = (event as TouchEvent).touches[0].clientY - boundingRect.top;
+      event.preventDefault();
+    } else if (
+      event.type === "touchend" &&
+      (event as TouchEvent).changedTouches?.length
+    ) {
+      canvasX =
+        (event as TouchEvent).changedTouches[0].clientX - boundingRect.left;
+      canvasY =
+        (event as TouchEvent).changedTouches[0].clientY - boundingRect.top;
+    } else {
+      canvasX = (event as MouseEvent).clientX - boundingRect.left;
+      canvasY = (event as MouseEvent).clientY - boundingRect.top;
+    }
     const forwardMatrix = rive.computeAlignment(
       fit,
       alignment,
@@ -31,7 +71,7 @@ export const registerTouchInteractions = ({
       },
       artboard.bounds
     );
-    let invertedMatrix = new rive.Mat2D();
+    const invertedMatrix = new rive.Mat2D();
     forwardMatrix.invert(invertedMatrix);
     const canvasCoordinatesVector = new rive.Vec2D(canvasX, canvasY);
     const transformedVector = rive.mapXY(
@@ -48,6 +88,7 @@ export const registerTouchInteractions = ({
 
     switch (event.type) {
       // Pointer moving/hovering on the canvas
+      case "touchmove":
       case "mouseover":
       case "mouseout":
       case "mousemove": {
@@ -57,6 +98,7 @@ export const registerTouchInteractions = ({
         break;
       }
       // Pointer click initiated but not released yet on the canvas
+      case "touchstart":
       case "mousedown": {
         for (const stateMachine of stateMachines) {
           stateMachine.pointerDown(transformedX, transformedY);
@@ -64,6 +106,7 @@ export const registerTouchInteractions = ({
         break;
       }
       // Pointer click released on the canvas
+      case "touchend":
       case "mouseup": {
         for (const stateMachine of stateMachines) {
           stateMachine.pointerUp(transformedX, transformedY);
@@ -73,17 +116,23 @@ export const registerTouchInteractions = ({
       default:
     }
   };
-  const callback = mouseCallback.bind(this);
+  const callback = processEventCallback.bind(this);
   canvas.addEventListener("mouseover", callback);
   canvas.addEventListener("mouseout", callback);
   canvas.addEventListener("mousemove", callback);
   canvas.addEventListener("mousedown", callback);
   canvas.addEventListener("mouseup", callback);
+  canvas.addEventListener("touchmove", callback);
+  canvas.addEventListener("touchstart", callback);
+  canvas.addEventListener("touchend", callback);
   return () => {
     canvas.removeEventListener("mouseover", callback);
     canvas.removeEventListener("mouseout", callback);
     canvas.removeEventListener("mousemove", callback);
     canvas.removeEventListener("mousedown", callback);
     canvas.removeEventListener("mouseup", callback);
+    canvas.removeEventListener("touchmove", callback);
+    canvas.removeEventListener("touchstart", callback);
+    canvas.removeEventListener("touchend", callback);
   };
 };
