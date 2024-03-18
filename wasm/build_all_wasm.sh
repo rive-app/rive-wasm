720e2fbb5bf977731e76fadf11e8358f3201310f
Chris Dalton <chris@rive.app>
2024-03-18 16:58:10 -0600

    Specify a branch name for checking out pls in rive-wasm

--- wasm/build_all_wasm.sh
+++ wasm/build_all_wasm.sh
@@ -3,56 +3,56 @@ set -e
 
 source ./get_emcc.sh
 
-echo
-echo "::::: building skia for wasm"
-echo
-path=$(readlink -f "${BASH_SOURCE:-$0}")
-DEV_SCRIPT_DIR=$(dirname $path)
-if [ -d "$DEV_SCRIPT_DIR/submodules/rive-cpp" ]; then
-    pushd submodules/rive-cpp/skia/dependencies
-else
-    pushd ../../runtime/skia/dependencies
-fi
-./make_skia_wasm.sh
-popd
-
-rm -f ../js/npm/canvas_advanced/*.mjs
-rm -f ../js/npm/canvas_advanced/*.wasm
-
-rm -f ../js/npm/canvas_advanced_lite/*.mjs
-rm -f ../js/npm/canvas_advanced_lite/*.wasm
-
-rm -f ../js/npm/webgl_advanced/*.mjs
-rm -f ../js/npm/webgl_advanced/*.wasm
-
-rm -f ../js/npm/canvas_advanced_single/*.mjs
-rm -f ../js/npm/webgl_advanced_single/*.mjs
-
-rm -f ../js/npm/canvas/*.mjs
-rm -f ../js/npm/canvas/*.wasm
-
-rm -f ../js/npm/canvas_lite/*.mjs
-rm -f ../js/npm/canvas_lite/*.wasm
-
-rm -f ../js/npm/webgl/*.mjs
-rm -f ../js/npm/webgl/*.wasm
-
-rm -f ../js/npm/canvas_single/*.mjs
-rm -f ../js/npm/webgl_single/*.mjs
+# echo
+# echo "::::: building skia for wasm"
+# echo
+# path=$(readlink -f "${BASH_SOURCE:-$0}")
+# DEV_SCRIPT_DIR=$(dirname $path)
+# if [ -d "$DEV_SCRIPT_DIR/submodules/rive-cpp" ]; then
+#     pushd submodules/rive-cpp/skia/dependencies
+# else
+#     pushd ../../runtime/skia/dependencies
+# fi
+# ./make_skia_wasm.sh
+# popd
+#
+# rm -f ../js/npm/canvas_advanced/*.mjs
+# rm -f ../js/npm/canvas_advanced/*.wasm
+#
+# rm -f ../js/npm/canvas_advanced_lite/*.mjs
+# rm -f ../js/npm/canvas_advanced_lite/*.wasm
+#
+# rm -f ../js/npm/webgl_advanced/*.mjs
+# rm -f ../js/npm/webgl_advanced/*.wasm
+#
+# rm -f ../js/npm/canvas_advanced_single/*.mjs
+# rm -f ../js/npm/webgl_advanced_single/*.mjs
+#
+# rm -f ../js/npm/canvas/*.mjs
+# rm -f ../js/npm/canvas/*.wasm
+#
+# rm -f ../js/npm/canvas_lite/*.mjs
+# rm -f ../js/npm/canvas_lite/*.wasm
+#
+# rm -f ../js/npm/webgl/*.mjs
+# rm -f ../js/npm/webgl/*.wasm
+#
+# rm -f ../js/npm/canvas_single/*.mjs
+# rm -f ../js/npm/webgl_single/*.mjs
 
 rm -f ../js/npm/webgl2_advanced/*.mjs
 rm -f ../js/npm/webgl2_advanced/*.wasm
 
-mkdir -p ../js/npm/webgl
-mkdir -p ../js/npm/canvas
-mkdir -p ../js/npm/canvas_lite
-mkdir -p ../js/npm/webgl_single
-mkdir -p ../js/npm/canvas_single
-mkdir -p ../js/npm/canvas_advanced
-mkdir -p ../js/npm/canvas_advanced_lite
-mkdir -p ../js/npm/webgl_advanced
-mkdir -p ../js/npm/canvas_advanced_single
-mkdir -p ../js/npm/webgl_advanced_single
+# mkdir -p ../js/npm/webgl
+# mkdir -p ../js/npm/canvas
+# mkdir -p ../js/npm/canvas_lite
+# mkdir -p ../js/npm/webgl_single
+# mkdir -p ../js/npm/canvas_single
+# mkdir -p ../js/npm/canvas_advanced
+# mkdir -p ../js/npm/canvas_advanced_lite
+# mkdir -p ../js/npm/webgl_advanced
+# mkdir -p ../js/npm/canvas_advanced_single
+# mkdir -p ../js/npm/webgl_advanced_single
 mkdir -p ../js/npm/webgl2
 mkdir -p ../js/npm/webgl2_advanced
 
@@ -62,54 +62,64 @@ echo "::::: cleaning all projects"
 echo
 rm -fR ./build
 
-echo
-echo "::::: building @rive-app/canvas_advanced"
-echo
-OUT_DIR=build/canvas_advanced/bin/release ./build_wasm.sh release
-cp build/canvas_advanced/bin/release/canvas_advanced.mjs ../js/npm/canvas_advanced/canvas_advanced.mjs
-cp build/canvas_advanced/bin/release/canvas_advanced.wasm ../js/npm/canvas_advanced/rive.wasm
-cp build/canvas_advanced/bin/release/canvas_advanced.wasm ../js/npm/canvas/rive.wasm
-cp ../js/src/rive_advanced.mjs.d.ts ../js/npm/canvas_advanced/rive_advanced.mjs.d.ts
+# echo
+# echo "::::: building @rive-app/canvas_advanced"
+# echo
+# OUT_DIR=build/canvas_advanced/bin/release ./build_wasm.sh release
+# cp build/canvas_advanced/bin/release/canvas_advanced.mjs ../js/npm/canvas_advanced/canvas_advanced.mjs
+# cp build/canvas_advanced/bin/release/canvas_advanced.wasm ../js/npm/canvas_advanced/rive.wasm
+# cp build/canvas_advanced/bin/release/canvas_advanced.wasm ../js/npm/canvas/rive.wasm
+# cp ../js/src/rive_advanced.mjs.d.ts ../js/npm/canvas_advanced/rive_advanced.mjs.d.ts
+#
+# echo
+# echo "::::: building @rive-app/canvas_advanced_lite"
+# echo
+# OUT_DIR=build/canvas_advanced_lite/bin/release ./build_wasm.sh -l release
+# cp build/canvas_advanced_lite/bin/release/canvas_advanced.mjs ../js/npm/canvas_advanced_lite/canvas_advanced.mjs
+# cp build/canvas_advanced_lite/bin/release/canvas_advanced.wasm ../js/npm/canvas_advanced_lite/rive.wasm
+# cp build/canvas_advanced_lite/bin/release/canvas_advanced.wasm ../js/npm/canvas_lite/rive.wasm
+# cp ../js/src/rive_advanced.mjs.d.ts ../js/npm/canvas_advanced_lite/rive_advanced.mjs.d.ts
+#
+# echo
+# echo "::::: building @rive-app/canvas_advanced_single"
+# echo
+# OUT_DIR=build/canvas_advanced_single/bin/release ./build_wasm.sh -s release
+# cp build/canvas_advanced_single/bin/release/canvas_advanced_single.mjs ../js/npm/canvas_advanced_single/canvas_advanced_single.mjs
+# cp ../js/src/rive_advanced.mjs.d.ts ../js/npm/canvas_advanced_single/rive_advanced.mjs.d.ts
+#
+# echo
+# echo "::::: building @rive-app/canvas_advanced_lite_single"
+# echo
+# OUT_DIR=build/canvas_advanced_lite_single/bin/release ./build_wasm.sh -l -s release
+# # We probably don't need to create a package for the lite+single version for canvas
+# # so no need to create a folder in npm to stage for publishing, but we'll keep the local
+# # build for testing purposes, and let webpack reference the wasm/build for this package here
+#
+# echo
+# echo "::::: building @rive-app/webgl_advanced"
+# echo
+# OUT_DIR=build/webgl_advanced/bin/release ./build_wasm.sh -r skia release
+# cp build/webgl_advanced/bin/release/webgl_advanced.mjs ../js/npm/webgl_advanced/webgl_advanced.mjs
+# cp build/webgl_advanced/bin/release/webgl_advanced.wasm ../js/npm/webgl_advanced/rive.wasm
+# cp build/webgl_advanced/bin/release/webgl_advanced.wasm ../js/npm/webgl/rive.wasm
+# cp ../js/src/rive_advanced.mjs.d.ts ../js/npm/webgl_advanced/rive_advanced.mjs.d.ts
+#
+# echo
+# echo "::::: building @rive-app/webgl_advanced_single"
+# echo
+# OUT_DIR=build/webgl_advanced_single/bin/release ./build_wasm.sh -r skia -s release
+# cp build/webgl_advanced_single/bin/release/webgl_advanced_single.mjs ../js/npm/webgl_advanced_single/webgl_advanced_single.mjs
+# cp ../js/src/rive_advanced.mjs.d.ts ../js/npm/webgl_advanced_single/rive_advanced.mjs.d.ts
 
 echo
-echo "::::: building @rive-app/canvas_advanced_lite"
+echo "::::: FIND1"
 echo
-OUT_DIR=build/canvas_advanced_lite/bin/release ./build_wasm.sh -l release
-cp build/canvas_advanced_lite/bin/release/canvas_advanced.mjs ../js/npm/canvas_advanced_lite/canvas_advanced.mjs
-cp build/canvas_advanced_lite/bin/release/canvas_advanced.wasm ../js/npm/canvas_advanced_lite/rive.wasm
-cp build/canvas_advanced_lite/bin/release/canvas_advanced.wasm ../js/npm/canvas_lite/rive.wasm
-cp ../js/src/rive_advanced.mjs.d.ts ../js/npm/canvas_advanced_lite/rive_advanced.mjs.d.ts
+find submodules
 
 echo
-echo "::::: building @rive-app/canvas_advanced_single"
+echo "::::: FIND2"
 echo
-OUT_DIR=build/canvas_advanced_single/bin/release ./build_wasm.sh -s release
-cp build/canvas_advanced_single/bin/release/canvas_advanced_single.mjs ../js/npm/canvas_advanced_single/canvas_advanced_single.mjs
-cp ../js/src/rive_advanced.mjs.d.ts ../js/npm/canvas_advanced_single/rive_advanced.mjs.d.ts
-
-echo
-echo "::::: building @rive-app/canvas_advanced_lite_single"
-echo
-OUT_DIR=build/canvas_advanced_lite_single/bin/release ./build_wasm.sh -l -s release
-# We probably don't need to create a package for the lite+single version for canvas
-# so no need to create a folder in npm to stage for publishing, but we'll keep the local
-# build for testing purposes, and let webpack reference the wasm/build for this package here
-
-echo
-echo "::::: building @rive-app/webgl_advanced"
-echo
-OUT_DIR=build/webgl_advanced/bin/release ./build_wasm.sh -r skia release
-cp build/webgl_advanced/bin/release/webgl_advanced.mjs ../js/npm/webgl_advanced/webgl_advanced.mjs
-cp build/webgl_advanced/bin/release/webgl_advanced.wasm ../js/npm/webgl_advanced/rive.wasm
-cp build/webgl_advanced/bin/release/webgl_advanced.wasm ../js/npm/webgl/rive.wasm
-cp ../js/src/rive_advanced.mjs.d.ts ../js/npm/webgl_advanced/rive_advanced.mjs.d.ts
-
-echo
-echo "::::: building @rive-app/webgl_advanced_single"
-echo
-OUT_DIR=build/webgl_advanced_single/bin/release ./build_wasm.sh -r skia -s release
-cp build/webgl_advanced_single/bin/release/webgl_advanced_single.mjs ../js/npm/webgl_advanced_single/webgl_advanced_single.mjs
-cp ../js/src/rive_advanced.mjs.d.ts ../js/npm/webgl_advanced_single/rive_advanced.mjs.d.ts
+find .
 
 echo
 echo "::::: building @rive-app/webgl2_advanced"
